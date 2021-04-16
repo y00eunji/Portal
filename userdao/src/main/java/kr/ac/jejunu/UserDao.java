5c445b83150057295805964e55279727bc6693eb
유은지 <aine0227@naver.com>
2021-04-16 17:11:23 +0900

    xml만듦

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import java.sql.*;
-
+//나는 주석이다
 public class UserDao {
     private final JdbcTemplate jdbcTemplate;
 
